b10ec6807eef22dc91293a3da87f1058dd26a604
Sunny-Muffin <vanuk@bk.ru>
2022-07-31 16:44:11 +0500

    Lesson4.5
    1. NavMesh запечен
    a) зона ходьбы есть
    b) зона медленной ходьбы тоже есть
    c) непроходимая зона есть
    2. Двери сделаны
    3. Турели работаю
    4. Противники спавнятся
    -5*. Вывод жизней НЕ сделал
    -6*. Инвентаря НЕТ
    7*. Противники теряют игрока из вида и возвращаются к патрулированию

--- Assets/Scripts/Enemy/FollowingEnemy.cs
+++ Assets/Scripts/Enemy/FollowingEnemy.cs
@@ -7,19 +7,62 @@ using UnityEngine.AI;
 
 public class FollowingEnemy : MonoBehaviour
 {
+    [SerializeField] private Transform[] waypoints;
+
     private NavMeshAgent agent;
     private Transform player;
+    private bool isFollowing = false;
+    private int index;
 
     // Start is called before the first frame update
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = FindObjectOfType<PlayerMovement>().transform;
+        agent.SetDestination(waypoints[0].position);
     }
 
-    // Update is called once per frame
     private void Update()
     {
+        if (NavMesh.SamplePosition(agent.transform.position, out NavMeshHit hit, 1f, NavMesh.AllAreas))
+        {
+            if (NavMesh.GetAreaCost((int)Mathf.Log(hit.mask, 2)) >= 4)
+                agent.speed = 1.5f;
+            else
+                agent.speed = 3;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isFollowing)
+            Follow();
+        else
+            Patrol();
+    }
+
+    private void Patrol ()
+    {
+        if (agent.remainingDistance < agent.stoppingDistance)
+        {
+            index = (index + 1) % waypoints.Length;
+            agent.SetDestination(waypoints[index].position);
+        }
+    }
+
+    private void Follow()
+    {
         agent.SetDestination(player.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+            isFollowing = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            isFollowing =false;
     }
 }
